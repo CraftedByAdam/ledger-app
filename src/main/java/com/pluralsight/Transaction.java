5153fdb2fbb71e4cb56f2c7a4c7f2dd8c3e63c4d
Your Name <youremail@example.com>
2026-04-27 09:22:59 -0400

    I made my Transactions class and added my constructor, getters and setters, and a toString method.

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,66 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
 public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private String name;
+    private String company;
+    private double amount;
+
+    public Transaction( LocalDate date, LocalTime time, String name, String company, double amount) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.amount = amount;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" + "date'" + date + "time" + time + "name " + name + ", company" + company + ", amount" + amount + '}';
+    }
 }
