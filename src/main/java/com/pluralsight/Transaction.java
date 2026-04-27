37f73c6442db84b40319c8a5c4de6a4a63ce39b9
Your Name <youremail@example.com>
2026-04-27 16:15:33 -0400

    I refactored the "name" and "company" to "description" and "vendor".
    I also removed my setters and kept only the getters. I learned that I should add setters only when I know I'll need it.
    
    I changed the toString to the right format.

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -2,65 +2,50 @@ package com.pluralsight;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 
 public class Transaction {
     private LocalDate date;
     private LocalTime time;
-    private String name;
-    private String company;
+    private String description;
+    private String vendor;// change to vendor
     private double amount;
 
-    public Transaction( LocalDate date, LocalTime time, String name, String company, double amount) {
-        this.name = name;
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
         this.date = date;
         this.time = time;
+        this.description = description;
+        this.vendor = vendor;
         this.amount = amount;
-        this.company = company;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public LocalTime getTime() {
         return time;
     }
 
-    public void setTime(LocalTime time) {
-        this.time = time;
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
     @Override
     public String toString() {
-        return "Transaction{" + "date'" + date + "time" + time + "name " + name + ", company" + company + ", amount" + amount + '}';
+        return "Transaction{" +
+                "date=" + date +
+                ", time=" + time +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
     }
 }
